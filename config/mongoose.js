4f9a0c9b3add5c6c8f15f7561b147af00279e539
soupaul_expedia <soupaul@expediagroup.com>
2025-05-10 18:23:43 +0530

    added the signin and signup APIs

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb+srv://soumyadeepsp:6QEtwtM4ioyOWSfF@hms.ku2litf.mongodb.net/?retryWrites=true&w=majority&appName=hms";
+const uri = "mongodb+srv://soumyadeepsp:<password>@hms.ku2litf.mongodb.net/?retryWrites=true&w=majority&appName=hms";
 
 mongoose.connect(uri).
     then(() => console.log("MongoDB connected successfully")).
